1bd25cbe1301f537aac8386f77c9726a4462b48f
ronaldo071515 <caceresderonaldo2015@gmail.com>
2025-12-20 15:52:45 -0500

    chore: typed cookies

--- src/app/(admin)/admin/live-classes/actions.ts
+++ src/app/(admin)/admin/live-classes/actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { createServerClient } from '@supabase/ssr'
+import { CookieOptions, createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
@@ -15,7 +15,13 @@ async function createServerSupabaseClient() {
                 getAll() {
                     return cookieStore.getAll()
                 },
-                setAll(cookiesToSet) {
+                setAll(
+                    cookiesToSet: {
+                        name: string
+                        value: string
+                        options?: CookieOptions
+                    }[]
+                ) {
                     try {
                         cookiesToSet.forEach(({ name, value, options }) => {
                             cookieStore.set(name, value, options)
